8d3b57b79f5e5a073550f2d12f1e2749cd0ef93b
Jacob Reid <reidwjacob@gmail.com>
2015-12-08 15:22:40 -0700

    cleaning tweats

--- src/Cluster.java
+++ src/Cluster.java
@@ -8,6 +8,7 @@ public class Cluster {
 	public static void main(String[] args[]) throws UnirestException {
 		// These code snippets use an open-source library.
 		// http://unirest.io/java
+		@SuppressWarnings("unused")
 		HttpResponse<JsonNode> response = Unirest.post("https://rxnlp-core.p.mashape.com/getTextFromHTML")
 				.header("X-Mashape-Key", "ZppTyXiFjxmshlUgnw78pZBfmKVGp1v6lxNjsnEbt7jwxUoMKh")
 				.header("Content-Type", "application/json").header("Accept", "application/json")
