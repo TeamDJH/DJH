6ed2d9a062b309248bd1bde3582ff794e105b714
Jacob Reid <reidwjacob@gmail.com>
2015-12-08 00:30:19 -0700

    Startedd clustering

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,4 +1,16 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class Cluster {
 
+	public static void main(String[] args[]) throws UnirestException {
+		// These code snippets use an open-source library.
+		// http://unirest.io/java
+		HttpResponse<JsonNode> response = Unirest.post("https://rxnlp-core.p.mashape.com/getTextFromHTML")
+				.header("X-Mashape-Key", "ZppTyXiFjxmshlUgnw78pZBfmKVGp1v6lxNjsnEbt7jwxUoMKh")
+				.header("Content-Type", "application/json").header("Accept", "application/json")
+				.body("{\"input\":\"http://www.rxnlp.com\",\"keepLineBreak\":\"false\"}").asJson();
+	}
 }
